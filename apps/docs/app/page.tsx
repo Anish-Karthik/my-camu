b66097d7b9653d55948f10d97777a4a53a024ae3
Anish Karthik <anishkarthika21cs@psnacet.edu.in>
2024-03-27 11:57:16 +0530

    Added elysia instead of hono and using eden for next frontend

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,9 +1,96 @@
+"use client";
 import { Button } from "@repo/ui/components/ui/button";
+import type { App } from "@repo/server";
+import { edenFetch, treaty } from "@elysiajs/eden";
+import { useEffect, useState } from "react";
+const api = treaty<App>("localhost:3000", {
+  fetch: {
+    credentials: "include",
+  },
+});
+const fetch = edenFetch<App>("http://localhost:3000");
+const arr = [
+  {
+    username: "user1",
+    password: "password1",
+  },
+  {
+    username: "user2",
+    password: "password2",
+  },
+  {
+    username: "admin",
+    password: "admin",
+  },
+];
 
 export default function Page() {
+  const [csrfToken, setCsrfToken] = useState("");
+  useEffect(() => {
+    fetch("/index", {
+      method: "GET",
+      credentials: "include",
+    }).then((response) => console.log(response.data, "response from server"));
+  }, []);
+  console.log(csrfToken);
   return (
-    <main>
-      <Button>Click me</Button>
-    </main>
+    <>
+      {arr.map((user) => (
+        <div key={user.username}>
+          <Button
+            onClick={() => {
+              api.login
+                .post({
+                  ...user,
+                })
+                .then((response) => {
+                  setCsrfToken(response.data.csrfToken);
+                  console.log(response.data);
+                  alert(JSON.stringify(response.data));
+                  alert(`${user.username} Logged In`);
+                })
+                .catch((err) => {
+                  console.log(err);
+                  alert(err);
+                });
+            }}
+          >
+            {user.username} login
+          </Button>
+        </div>
+      ))}
+      <Button
+        onClick={() => {
+          api.logout
+            .post({
+              headers: {
+                csrfToken,
+              },
+            })
+            .then(() => {
+              alert("Logged out");
+              setCsrfToken("");
+            })
+            .catch((err) => alert(err));
+        }}
+      >
+        Logout
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("profile", csrfToken);
+          api.profile
+            .get({
+              headers: {
+                csrfToken: csrfToken,
+              },
+            })
+            .then((response) => alert(JSON.stringify(response.data)))
+            .catch((err) => alert(err));
+        }}
+      >
+        Profile
+      </Button>
+    </>
   );
 }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,228 +1,208 @@
-import { Hono, MiddlewareHandler } from "hono";
-import { verifyRequestOrigin } from "lucia";
-import { cors } from "hono/cors";
-import { csrf } from "hono/csrf";
-import { db, lucia } from "./lib/auth.js";
-import type { Session, User } from "lucia";
-import { Argon2id } from "oslo/password";
+import { cors } from "@elysiajs/cors";
 import { randomBytes } from "crypto";
-type Variables = {
-  session: Session | null;
-  user: User | null;
-};
+import { Elysia, error, t } from "elysia";
+import { Argon2id } from "oslo/password";
+import { db, lucia } from "./lib/auth.js";
+
 const origin = [
-  "http://localhost:5173",
-  "http://localhost:3000",
-  "http://localhost:3001",
-  "http://localhost:3002",
+  /http:\/\/localhost:*/,
+  /.*\.anish-karthik\.site$/,
+  /.*\.anish-karthik\.vercel\.app$/,
 ];
-const app = new Hono<{ Variables: Variables }>({});
-app.use(
-  cors({
-    origin,
-    allowHeaders: ["X-Custom-Header", "Upgrade-Insecure-Requests"],
-    allowMethods: ["POST", "GET", "OPTIONS"],
-    exposeHeaders: ["Content-Length", "X-Kuma-Revision"],
-    maxAge: 600,
-    credentials: true,
-  }),
+
+const authCookie = new Elysia({ name: "authCookie" }).guard((app) =>
+  app
+    .derive(async (c) => {
+      const auth = c.headers["authorization"];
+      console.log("authCookie");
+      const sessionId = lucia.readSessionCookie(c.headers.cookie ?? "");
+      console.log(sessionId);
+      if (!sessionId) {
+        console.log("no session id");
+
+        return {
+          session: null,
+          user: null,
+          bearer: auth?.startsWith("Bearer ") ? auth.slice(7) : null,
+        };
+      }
+
+      const { session, user } = await lucia.validateSession(sessionId);
+      console.log(session);
+      console.log(user);
+      if (session && session.fresh) {
+        console.log("fresh session");
+        c.set.headers["set-sookie"] = lucia
+          .createSessionCookie(session.id)
+          .serialize();
+      }
+
+      if (!session) {
+        c.set.headers["set-cookie"] = lucia
+          .createBlankSessionCookie()
+          .serialize();
+      }
+
+      return {
+        session,
+        user,
+        bearer: auth?.startsWith("Bearer ") ? auth.slice(7) : null,
+      };
+    })
+    .post("/logout", async (c) => {
+      console.log("logout");
+      console.log(c.session, c.user);
+      try {
+        if (!c.session) {
+          return error(401, { message: "Unauthenticated" });
+        }
+        await lucia.invalidateSession(c.session.id);
+        c.set.headers["set-cookie"] = lucia
+          .createBlankSessionCookie()
+          .serialize();
+        return { message: "Logged out" };
+      } catch (err: any) {
+        console.log(err.message);
+        return error(500, { message: "Internal server error" });
+      }
+    })
+    .get("/profile", async (c) => {
+      console.log("profile");
+      console.log(c.user);
+      try {
+        if (!c.user) {
+          return error(401, { message: "Unauthenticated" });
+        }
+        return c.user;
+      } catch (error: any) {
+        console.log(error.message);
+        return error(500, { message: "Internal server error" });
+      }
+    }),
 );
-app.use(csrf());
-// string[]
-app.use(
-  csrf({
-    origin,
-  }),
+
+const app = new Elysia()
+  .use(authCookie)
+  .use(
+    cors({
+      origin,
+      maxAge: 600,
+      credentials: true,
+      allowedHeaders: [
+        "*",
+        "Authorization",
+        "Accept",
+        "Content-Type",
+        "Origin",
+        "set-cookie",
+        "Set-Cookie",
+        "csrftoken",
+      ],
+      methods: ["GET", "POST", "OPTIONS"],
+    }),
+  )
+  .get("/", () => "Landing")
+  .get("/ping", () => "pong")
+  .guard(
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String(),
+      }),
+      cookie: t.Object({
+        test: t.String(),
+      }),
+    },
+    (app) =>
+      app
+
+        .post("/signup", async (c) => {
+          console.log("signup");
+          try {
+            const body = c.body;
+
+            const { username, password } = body;
+            console.log(username, password);
+            if (!username || !password) {
+              return error(400, "Invalid username or password");
+            }
+            if (await db.user.findUnique({ where: { username } })) {
+              console.log("Username already exists");
+              return error(400, "Username already exists");
+            }
+            const hashedPassword = await new Argon2id().hash(password);
+            const user =
+              (await db.user.findUnique({ where: { username } })) ||
+              (await db.user.create({
+                data: {
+                  username,
+                  password: hashedPassword,
+                },
+              }));
+            console.log(user);
+            const sessionId = randomBytes(12).toString("hex");
+            const session = await lucia.createSession(
+              user.id,
+              {},
+              {
+                sessionId,
+              },
+            );
+            console.log(session);
+            c.cookie.test.value = "testing";
+            const token = lucia.createSessionCookie(session.id);
+            console.log(token);
+            c.set.headers["set-cookie"] = token.serialize();
+            c.set.status = 200;
+            console.log(c.headers);
+            return { session: session.id, user: user.id };
+          } catch (error: any) {
+            console.log(error.message);
+            return c.error(500, { message: "Internal server error" });
+          }
+        })
+        .post("/login", async (c) => {
+          try {
+            const { username, password } = c.body;
+            console.log(username, password);
+            if (!username || !password) {
+              return error(400, "Invalid username or password");
+            }
+            const user = await db.user.findUnique({
+              where: {
+                username,
+              },
+            });
+
+            if (!user) {
+              return error(401, "Invalid username or password");
+            }
+            const isValidPassword = await new Argon2id().verify(
+              user.password,
+              password,
+            );
+            if (isValidPassword) {
+              const sessionId = randomBytes(12).toString("hex");
+              const session = await lucia.createSession(
+                user.id,
+                {},
+                { sessionId },
+              );
+              const token = lucia.createSessionCookie(session.id);
+              c.set.headers["set-cookie"] = token.serialize();
+              return { session: session.id, user: user.id };
+            } else {
+              return error(401, "Invalid username or password");
+            }
+          } catch (error: any) {
+            console.log(error.message);
+            return error(500, "Internal server error");
+          }
+        }),
+  )
+
+  .listen(3000);
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
-// alternate to cors
-app.use(async (c, next) => {
-  console.log(c.req.method, c.req.url);
-  if (c.req.method === "GET") {
-    await next();
-    return;
-  }
-  // return;
-  const originHeader = c.req.header().origin ?? null;
-  const hostHeader = c.req.header().host ?? null;
-
-  console.log(originHeader, hostHeader);
-  // return;
-  console.log(c.req.header().cookie);
-  console.log(c.req);
-  // return;
-  const body = await c.req.json();
-  // return;
-  console.log(body?.username, body?.password);
-  if (
-    !originHeader ||
-    !hostHeader ||
-    !verifyRequestOrigin(originHeader, [hostHeader, "http://localhost:5173"])
-  ) {
-    c.json({ message: "Forbidden" }, 403);
-    console.log("forbidden");
-    return;
-  }
-  await next();
-});
-
-const authCookie: MiddlewareHandler<{
-  Variables: {
-    session: Session | null;
-    user: User | null;
-  };
-}> = async (c, next) => {
-  console.log("authCookie");
-  const sessionId = lucia.readSessionCookie(c.req.header().cookie ?? "");
-  console.log(sessionId);
-  if (!sessionId) {
-    console.log("no session id");
-    c.set("session", null);
-    c.set("user", null);
-    await next();
-    return;
-  }
-
-  const { session, user } = await lucia.validateSession(sessionId);
-  console.log(session);
-  console.log(user);
-  if (session && session.fresh) {
-    console.log("fresh session");
-    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize());
-  }
-
-  if (!session) {
-    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize());
-  }
-
-  c.set("session", session);
-  c.set("user", user);
-  await next();
-};
-
-// app.use(authCookie);
-
-app.get("/", async (c) => {
-  console.log("root");
-  return c.json({
-    message: `Hey, did you know that 2 + 2 = ${4}. I know right?`,
-  });
-});
-app.get("/ping", async (c) => c.json({ message: "pong" }));
-
-app.post("/signup", async (c) => {
-  console.log("signup");
-  const body = await c.req.json();
-
-  try {
-    const { username, password } = body;
-    console.log(username, password);
-    if (!username || !password) {
-      return c.json({ message: "Invalid request" }, 400);
-    }
-    if (await db.user.findUnique({ where: { username } })) {
-      return c.json({ message: "Username already exists" }, 400);
-    }
-    const hashedPassword = await new Argon2id().hash(password);
-    const user = await db.user.create({
-      data: {
-        username,
-        password: hashedPassword,
-      },
-    });
-
-    console.log(user);
-    // for mongodb compatibility, not recommended
-    // other change is possible in the creating mongoId
-    const sessionId = randomBytes(12).toString("hex");
-    const session = await lucia.createSession(
-      user.id,
-      {},
-      {
-        sessionId,
-      },
-    );
-    c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize());
-    return c.json({ message: "success" }, 200);
-  } catch (error: any) {
-    console.log(error.message);
-    return c.json({ message: error.message }, 500);
-  }
-});
-
-app.post("/login", async (c) => {
-  try {
-    const { username, password } = await c.req.json();
-    console.log(username, password);
-    if (!username || !password) {
-      c.json({ message: "Invalid request" }, 400);
-      return;
-    }
-    const user = await db.user.findUnique({
-      where: {
-        username,
-      },
-    });
-    if (!user) {
-      return c.json({ message: "Invalid username or password" }, 401);
-    }
-    const isValidPassword = await new Argon2id().verify(
-      user.password,
-      password,
-    );
-    if (isValidPassword) {
-      const session = await lucia.createSession(user.id, {});
-      c.header("Set-Cookie", lucia.createSessionCookie(session.id).serialize());
-      return c.json({ message: "success" }, 200);
-      // TODO: send CSRF token
-      // c.json({ csrfToken: session.csrfToken });
-    } else {
-      return c.json({ message: "Invalid username or password" }, 401);
-    }
-  } catch (error: any) {
-    console.log(error.message);
-    return c.json({ message: "Internal server error" }, 500);
-  }
-});
-
-app.post("/logout", authCookie, async (c) => {
-  console.log("logout");
-  try {
-    if (!c.var.session) {
-      return c.json({ message: "Unauthenticated" }, 401);
-    }
-    await lucia.invalidateSession(c.var.session.id);
-    c.header("Set-Cookie", lucia.createBlankSessionCookie().serialize());
-    console.log(c);
-    return c.json({ message: "Logged out" }, 200);
-  } catch (error: any) {
-    // console.log(error.message);
-    return c.json({ message: "Internal server error" }, 500);
-  }
-});
-
-app.get("/profile", authCookie, async (c) => {
-  try {
-    if (!c.var.session) {
-      return c.json({ message: "Unauthenticated" }, 401);
-    }
-    return c.json(c.var.user);
-  } catch (error: any) {
-    console.log(error.message);
-    return c.json({ message: "Internal server error" }, 500);
-  }
-});
-
-export default {
-  ...app,
-  port: 3000,
-  fetch: app.fetch,
-};
-
-declare global {
-  namespace Hono {
-    interface Locals {
-      user: User | null;
-      session: Session | null;
-    }
-  }
-}
+export type App = typeof app;
